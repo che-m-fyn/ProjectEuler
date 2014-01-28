1eb1799cc270e723636a388600c0efd26b057b0c
fynmi <m.isobe.fyn@gmail.com>
2014-01-29 00:27:40 +0900

    Modify

--- prb0001.c
+++ prb0001.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
 
-void main(int argc, char *argv[]) {
-    int sum = 0;
-    int div3, div5, div15;
-    int sum_cntr;
+int main (void)
+{
+    int ans = 0;
 
-    div3 = 999 / 3;
-    div5 = 999 / 5;
-    div15 = 999 / 15;
-
-    for (sum_cntr = 1; sum_cntr <= div3; sum_cntr++) {
-        sum += 3 * sum_cntr;
+    for (int i = 3; i < 1000; i++) {
+        if ((i % 3 == 0) || (i % 5 == 0)) {
+            ans += i;
+        }
     }
 
-    for (sum_cntr = 1; sum_cntr <= div5; sum_cntr++) {
-        sum += 5 * sum_cntr;
-    }
-
-    for (sum_cntr = 1; sum_cntr <= div15; sum_cntr++) {
-        sum -= 15 * sum_cntr;
-    }
-
-    printf("The sum is %d\n", sum);
+    printf("answer=%d\n", ans);
+    return 0;
 }
