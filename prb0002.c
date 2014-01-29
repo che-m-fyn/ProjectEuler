ae25b716c4c817307dad1a9627d7d8f80a46a56a
fynmi <m.isobe.fyn@gmail.com>
2014-01-29 23:13:02 +0900

    Modify

--- prb0002.c
+++ prb0002.c
@@ -1,21 +1,24 @@
-#include <stdio.h>
+#include<stdio.h>
 
-void main (int argc, char *argv[]) {
-    
+int main (void)
+{
+    int sum = 0;
+
     int term1 = 1;
     int term2 = 2;
     int tmp_term2 = 0;
-    int sum = 0;
-
+    
     while (term2 < (4 * 1000 * 1000)) {
-        if (term2%2 == 0) {
+    
+        if (term2 % 2 == 0) {
             sum += term2;
         }
-        tmp_term2 = term2; 
+
+        tmp_term2 = term2;
         term2 = term1 + term2;
         term1 = tmp_term2;
     }
-
-    printf("sum of even terms = %d\n", sum);
-    return;
+    printf ("answer=%d\n", sum);
+    return 0;
 }
+
